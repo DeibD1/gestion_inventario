d51ac42c08f2190329f40af53154b97d2ae6654c
Hammer <titimendoza31@gmail.com>
2025-03-07 18:36:21 -0500

    Modificacion en el footer

--- views/shared/footer.php
+++ views/shared/footer.php
@@ -3,7 +3,6 @@
         Desarrollado por <span class="fw-bold">El bicho SIU</span> .::. &copy; 2023
 </footer> -->
 <p style="text-align: center">FOOTER DEL PROYECTO</p>
-<p style="text-align: center">HAMMER NO HIZO NA</p>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"></script>
