1b51b8e5843227f1d0f6d55e703d1f3b4f3ff6ba
DeibD1 <juandavidacostabasto8@gmail.com>
2025-03-07 19:40:38 -0500

    Crea el login

---
+++ views/producto/registration.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+</head>
+<body>
+    <div class="container">
+        <form action="registration.php" method="post">
+            <div class="form-group">
+                <input type="text" name="fullname" placeholder="Nombre">
+            </div>
+            <div class="form-group">
+                <input type="text" name="email" placeholder="Email">
+            </div>
+            <div class="form-group">
+                <input type="phone" name="phone" placeholder="Phone">
+            </div>
+            <div class="form-group">
+                <input type="" name="fullname" placeholder="Nombre">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
