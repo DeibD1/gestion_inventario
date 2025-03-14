9685c168b6ee378a6fea9d0825837f17a57b2f6f
Brandon Cardona <cardonamartinezb@gmail.com>
2025-03-13 20:26:47 -0500

    Correccion en la barra de navegacion

--- views/proveedor/insert.php
+++ views/proveedor/insert.php
@@ -1,4 +1,16 @@
-<?php require_once "views/shared/header.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion-inventario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="./assets/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    
+<?php require_once "views/shared/navbar.php"; ?>
+
 
 <form action="index.php?controlador=Proveedor&accion=store" method="post" >
 
